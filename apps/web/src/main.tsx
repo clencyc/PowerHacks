93be7d3200d4d3fb5f794fb9159b3a38dfd38c28
Egenue <gene23.chiki@gmail.com>
2025-11-29 18:35:59 +0300

    Attempt 2

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,10 @@
-import 'react/jsx-runtime';
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,20 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    allowedHosts: ["powerhacks-2.onrender.com"],
-  },
-  plugins: [react({
-    jsxRuntime: 'automatic'
-  })],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
